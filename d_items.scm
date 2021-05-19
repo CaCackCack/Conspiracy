0563236426e9d18e96edcd63bde7bb34f53e9d2c
CaCackCack <cacackcack@protonmail.ch>
2021-05-19 12:36:13 -0400

    still trying to figure out how to translate #pragma directives to Scheme. Any help is appreciated!.

---
+++ d_items.scm
@@ -0,0 +1,7 @@
+
+
+(if (environment-bound? (nearest-repl/environment) '__D_ITEMS__) (define __D_ITEMS__) )
+
+(define weaponinfo_t (list ammo upstate downstate readystate atkstate flashstate) )
+
+ 
---
+++ r_sky.scm
@@ -0,0 +1,16 @@
+
+
+
+(if (environment-bound? (nearest-repl/environment) '__R_SKY__) (define __R_SKY__) )
+
+
+(define SKYFLATNAME "F_SKY1")
+
+(define ANGLETOKEYSHIFT 22)
+
+(define skytexture)
+(define skytexturemid)
+
+(define (R_InitSkyMap ) )
+
+
